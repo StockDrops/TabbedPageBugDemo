663361f314afcc36fb96fd3ed8c4bc4177598d84
Henry <emorell96@hotmail.com>
2021-12-12 00:52:54 -0800

    Add project files.

---
+++ TabbedPageExample.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace TabbedPageBugDemo
+{
+	public partial class TabbedPageExample : TabbedPage
+	{
+		int count = 0;
+
+		public TabbedPageExample()
+		{
+			InitializeComponent();
+		}
+	}
+}
